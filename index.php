6cc72aa014a848d33fb220ab9ff1a7621db89400
Nurul Auliya <gfn.aly@gmail.com>
2023-10-16 16:56:45 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>hello world</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus natus sapiente iusto harum quisquam assumenda tempora quas non voluptas sint id, similique beatae incidunt atque molestiae eos explicabo nihil totam.</p>
 </body>
 </html>
