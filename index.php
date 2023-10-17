c67b77149145c9b2bd2a9b7236939b0e304c7c83
Nurul Auliya <gfn.aly@gmail.com>
2023-10-17 15:59:49 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>hello world</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus natus sapiente iusto harum quisquam assumenda tempora quas non voluptas sint id, similique beatae incidunt atque molestiae eos explicabo nihil totam.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
